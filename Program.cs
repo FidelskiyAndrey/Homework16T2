409ff712a728424b93cee402f4d0efbf894237da
FidelskiyAndrey <fidelskiy@gmail.com>
2021-11-04 21:19:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+namespace Homework16T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.StreamReader f = new StreamReader(@"C:\Users\andrey\Serialize.json");
+            string jsonproduct = f.ReadToEnd();
+
+            
+            Product product = JsonSerializer.Deserialize<Product>(jsonproduct);
+
+
+
+
+            Console.WriteLine(jsonproduct);
+            Console.ReadKey();
+        }
+    }
+    public class Product
+    {
+
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+        public Product(int code, string name, double price)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+        }
+    }
+
+
+
+}
